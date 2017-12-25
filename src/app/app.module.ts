ceda16a39551dba908b259b477977369bbfbd55c
daliyajohn <daliyajohn75@gmail.com>
2017-12-25 11:02:35 +0530

    Add  app layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppLoginModule } from '../container/app-login/app-login.module';
+import { AppLayoutComponent } from '../container/app-layout/app-layout.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,6 @@ import { AppLoginModule } from '../container/app-login/app-login.module';
     AppLoginModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AppLayoutComponent]
 })
 export class AppModule { }
