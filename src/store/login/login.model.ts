1820caf8751f67b41463f076990f9a070ec5e840
daliyajohn <daliyajohn75@gmail.com>
2017-12-26 21:27:08 +0530

    Integrate model

---
+++ src/store/login/login.model.ts
@@ -0,0 +1,12 @@
+export interface User {
+    username?: string;
+    password?: string;
+}
+export interface Authenticated {
+    users: UserObject;
+}
+
+export interface UserObject {
+    error: boolean;
+    message: any;
+}
